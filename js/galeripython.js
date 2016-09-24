4478fb4788a444fd2c7bfd3bb84abeaad1f5d345
iwannabd <azaqokay@gmail.com>
2016-09-24 09:16:47 +0700

    init

---
+++ js/galeripython.js
@@ -0,0 +1,5 @@
+for (i = 1; i < 15; i++){
+    document.write("<div class='item'>" +
+        "<img class='itemgalery' src='img/fotopython/"+i+".jpg' >" +
+        "</div>")
+}
---
+++ js/galerisage.js
@@ -0,0 +1,5 @@
+for (i = 1; i < 7; i++){
+    document.write("<div class='item'>" +
+        "<img class='itemgalery' src='img/fotosage/"+i+".jpg' >" +
+        "</div>")
+}
