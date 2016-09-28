9a0588bbef56799320f1bde7f9525ea68df84a7f
iwannabd <azaqokay@gmail.com>
2016-09-28 19:26:10 +0700

    auth beres sekarang masih kerjain panel post

--- js/panelapp.js
+++ js/panelapp.js
@@ -10,25 +10,80 @@ var config = {
 firebase.initializeApp(config);
 
 app.run(function ($rootScope,$timeout) {
-    $rootScope.$on('$viewContentLoaded', function(){
-        $timeout(function() {
-        componentHandler.upgradeAllRegistered();
+        $rootScope.$on('$viewContentLoaded', function(){
+            $timeout(function() {
+            componentHandler.upgradeAllRegistered();
+        })
     })
-})
 });
+
+app.run(["$rootScope", "$location", function($rootScope, $location) {
+    $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
+        // We can catch the error thrown when the $requireSignIn promise is rejected
+        // and redirect the user back to the home page
+        if (error === "AUTH_REQUIRED") {
+            $location.path("/");
+            window.alert("login dulu cuy")
+        }
+    });
+}]);
 
 app.config(function ($routeProvider) {
     $routeProvider
-        .when("/login",{
+        .when("/",{
             controller:"LoginCtrl",
-            templateUrl:"template/login.htm"
-        });
+            templateUrl:"template/login.htm",
+            resolve:{
+                "currentAuth": ["Auth",function (Auth) {
+                    return Auth.$waitForSignIn();
+                }]
+            }
+        })
+        .when("/post",{
+            controller:"PostCtrl",
+            templateUrl:"template/posts.htm",
+            resolve:{
+                "currentAuth": ["Auth", function (Auth) {
+                    return Auth.$requireSignIn();
+                }]
+            }
+        })
 });
 
-app.controller("LoginCtrl",function ($scope) {
+app.controller("headerCtrl",function (Auth, $scope, $location) {
+    $scope.logout = function () {
+        Auth.$signOut();
+        $location.path('/');
+    }
+});
+
+app.controller("LoginCtrl",function ($scope,$firebaseAuth,$location,currentAuth) {
+    var auth = $firebaseAuth();
+    console.log(currentAuth);
+    if (currentAuth){
+        $location.path('/post')
+    }
 
     $scope.loginbtn = function () {
         console.log($scope.user);
+        var user = $scope.user;
+        auth.$signInWithEmailAndPassword(user.email,user.pass).then(function (firebaseUser) {
+            $location.path('/post')
+        }).catch(function (error) {
+            console.log(error);
+            alert(" Password salah cuy, atau belum kedaftar")
+        })
     }
 
 });
+
+app.controller("PostCtrl",function ($scope,currentAuth) {
+    $scope.saha = currentAuth;
+
+});
+
+app.factory("Auth", ["$firebaseAuth",
+    function($firebaseAuth) {
+        return $firebaseAuth();
+    }
+]);
